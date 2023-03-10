f3b1b94829d1aa8e9cfe934e57b961507b405e66
Johannes Kutning <120175758+JohannesKutning@users.noreply.github.com>
2023-03-10 21:23:09 +0100

    Adds seedable random number and wordlist
    The randomizer takes a seed and index to create repoducable wordlists

--- backend/src/randomizer.rs
+++ backend/src/randomizer.rs
@@ -1,13 +1,37 @@
+use rand_seeder::Seeder;
+use rand::{rngs::StdRng, Rng, SeedableRng};
+use crate::wordlist::get_wordlist;
 pub struct Randomizer {
+    rng: StdRng,
 }
 
 impl Randomizer {
-    pub fn new(seed: &str) -> Self {
-        Self {}
+    pub fn new(seed: &str, idx: &str) -> Self {
+        let mut seed = seed.to_string();
+        seed.push_str( idx );
+        Self {rng: Seeder::from("stripy zebra").make_rng()}
     }
 
-    pub fn words(&mut self, count: u32) -> Vec<String> {
-        let words =["petite".to_string(), "smugly".to_string(), "earmuff".to_string(), "sneaker".to_string()];
-        words.to_vec()
+    pub fn words(&mut self, cnt: u32) -> Vec<String> {
+        let mut words: Vec<String> = Vec::new();
+        for _ in 0..cnt {
+            words.push(self.generate_word());
+        }
+        words
+    }
+
+    fn generate_word(&mut self) -> String {
+        let mut number: u32 = 0;
+
+        for _ in 0..5 {
+            let dice_roll = self.rng.gen_range(1..7);
+            number *= 10;
+            number += dice_roll;
+        }
+
+        get_wordlist()
+            .get_word(number)
+            .unwrap()
+            .to_string()
     }
 }
