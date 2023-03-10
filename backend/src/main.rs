8d37aa50ca02502ac3431e09207d0148b4ba6036
Johannes Kutning <120175758+JohannesKutning@users.noreply.github.com>
2023-03-10 23:54:02 +0100

    Adds http interface
    The interface parses for seed=<seed>&index=<number> in the http GET

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -31,30 +31,36 @@ async fn main() -> std::io::Result<()> {
 
 #[allow(clippy::unused_io_amount)]
 async fn handle_connection(mut stream: TcpStream) {
-    // handle the connection
-
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-
-    // check if the request is a GET request
     if buffer.starts_with(b"GET") {
-        let mut randomizer = Randomizer::new("seed", "0");
-
-        let words = randomizer.words(4);
-
-        let mut video_provider = VideoProvider::new(&words);
-        let video = video_provider.get_video_from_words().await.unwrap();
-
-        let video_json = serde_json::to_string(&video).unwrap();
-
-        let response = format!(
-            "HTTP/1.1 200 OK\r
-
-{}",
-            video_json
-        );
-
+        let response = handle_request(&buffer).await;
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
     }
+}
+
+async fn handle_request(buffer: &[u8]) -> String {
+    let buffer_str = String::from_utf8( buffer.to_vec() ).unwrap();
+    let re = regex::Regex::new(r#"GET /\?seed=(.+)&index=(.+) HTTP.*"#).unwrap();
+    if !re.is_match(&buffer_str) {
+        let response = "HTTP/1.1 400 Bad Request\r
+".to_string();
+        return response;
+    }
+    let captures = re.captures( &buffer_str ).unwrap();
+    let seed = captures.get( 1 ).map_or( "", |m| m.as_str() );
+    let idx = captures.get( 2 ).map_or( "", |m| m.as_str() );
+    let mut randomizer = Randomizer::new(seed, idx);
+    let words = randomizer.words(4);
+    let mut video_provider = VideoProvider::new(&words);
+    let video = video_provider.get_video_from_words().await.unwrap();
+    let video_json = serde_json::to_string(&video).unwrap();
+    let response = format!(
+        "HTTP/1.1 200 OK\r
+
+{}",
+        video_json
+    );
+    response
 }
